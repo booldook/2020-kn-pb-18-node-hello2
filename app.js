a87eb47e6c1ea0de403ead2e4daeefac47565093
booldook <booldook@gmail.com>
2020-07-10 17:00:14 +0900

    post 연동

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const path = require('path');
 /*********** 서버 구동 *************/
 app.listen(3000, () => { console.log('http://127.0.0.1:3000') });
 
+/*********** JSON, req.body 파싱 *************/
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 /*********** 정적폴더 등록 *************/
 app.use('/', express.static(path.join(__dirname, './public')));
 
@@ -23,5 +27,10 @@ app.get('/user/save', (req, res) => {
 	// const user = {...req.query}
 	// res.send(`${user.userid} / ${user.userpw} / ${user.username} / ${user.email}`);
 	const { userid, userpw, username, email } = req.query;
+	res.send(`<h1>${userid} / ${userpw} / ${username} / ${email}</h1>`);
+});
+
+app.post('/user/save', (req, res) => {
+	const { userid, userpw, username, email } = req.body;
 	res.send(`<h1>${userid} / ${userpw} / ${username} / ${email}</h1>`);
 });
